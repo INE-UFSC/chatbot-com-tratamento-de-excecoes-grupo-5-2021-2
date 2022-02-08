fb6319b8555a0b4f606bc56faa69dd319429909f
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-02-08 11:43:50 -0300

    Adição de try except realizada durante a aula

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
-from Bots.BotZangado import BotZangado
+from Bots.BotMarombeiro import BotMarombeiro
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+lista_bots = [BotMarombeiro("Bambam")]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
